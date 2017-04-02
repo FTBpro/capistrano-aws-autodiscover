607663cc07be80e43832ed3aeaf0ff9b5ccb6628
Liz Gorbonos <liz.g@minutemedia.com>
2017-04-02 14:54:26 +0300

    Support Capistrano 3

--- lib/cap-aws-ec2.rb
+++ lib/cap-aws-ec2.rb
@@ -1,14 +1,16 @@
 require "cap-aws-ec2/version"
 require 'aws-sdk-core'
 
-Capistrano::Configuration.instance(:must_exist).load do
-  def define_servers
-    instances = CapAwsEc2.new(fetch(:aws_key_id), fetch(:secret_access_key), fetch(:aws_region), fetch(:ec2_project), fetch(:ec2_env)).execute
-    instances.each {|s| server *s}
-  end
-end
-
 class CapAwsEc2
+
+  Instance = Struct.new(:dns, :roles)
+
+  def self.define_servers
+    instances = CapAwsEc2.new(fetch(:aws_key_id), fetch(:secret_access_key), fetch(:aws_region), fetch(:ec2_project), fetch(:ec2_env)).execute
+    instances.each do |s| 
+      server s.dns, roles: s.roles, user: fetch(:user)
+    end
+  end
 
   def initialize(key, secret, aws_region, project, environment)
     @key = key
@@ -39,7 +41,7 @@ class CapAwsEc2
               Aws.config = { access_key_id: @key,
                              secret_access_key: @secret,
                              region: @region }
-              Aws::EC2.new
+              Aws::EC2::Client.new
              end
   end
 
@@ -57,10 +59,10 @@ class CapAwsEc2
   end
 
   def make_args(instance)
-    ip = instance.public_ip_address
+    dns = instance.public_dns_name
     roles_tag = instance.tags.find {|t| t.key == "Roles"}.value
     roles = roles_tag.split(/,|;/)
-    [ip, roles].flatten
+    Instance.new(dns, roles)
   end
 
 end
