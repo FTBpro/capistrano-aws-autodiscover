63acf5d5a8e0d020603555bad904a0fcfd7bcf24
Liz Gorbonos <liz.g@minutemedia.com>
2017-05-12 14:08:24 +0300

    change gem name and file names

--- lib/cap-aws-ec2/version.rb
+++
@@ -1,3 +0,0 @@
-class CapAwsEc2
-  VERSION = "0.0.1"
-end
--- lib/capistrano-aws-autodiscover.rb
+++ lib/capistrano-aws-autodiscover.rb
@@ -1,14 +1,14 @@
-require "cap-aws-ec2/version"
+require "capistrano-aws-autodiscover/version"
 require 'aws-sdk-core'
 
 Capistrano::Configuration.instance(:must_exist).load do
   def define_servers
-    instances = CapAwsEc2.new(fetch(:aws_key_id), fetch(:secret_access_key), fetch(:aws_region), fetch(:ec2_project), fetch(:ec2_env)).execute
+    instances = CapistranoAwsAutodiscover.new(fetch(:aws_key_id), fetch(:secret_access_key), fetch(:aws_region), fetch(:ec2_project), fetch(:ec2_env)).execute
     instances.each {|s| server *s}
   end
 end
 
-class CapAwsEc2
+class CapistranoAwsAutodiscover
 
   def initialize(key, secret, aws_region, project, environment)
     @key = key
